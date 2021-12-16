37a3ab0ad0e19f8a1d02aba946b362a9e534e9e5
Zimmer-Bentin, Karl-Dieter (GOD) <karl-dieter.zimmer-bentin@god.de>
2021-12-16 10:45:36 +0100

    puzzle 7 part 1 and 2 solved

--- src/com/example/AssemblyRequired.java
+++ src/com/example/AssemblyRequired.java
@@ -1,9 +1,7 @@
 package com.example;
 
 import lombok.Builder;
-import lombok.NonNull;
 import lombok.SneakyThrows;
-import lombok.ToString;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -17,102 +15,71 @@ public class AssemblyRequired {
     @SneakyThrows
     public static void main(String[] args) {
 
-        var url = AssemblyRequired.class.getResource("/simplecircuit.txt");
+        var url = AssemblyRequired.class.getResource("/assemblyinstructions.txt");
         var path = Paths.get(Objects.requireNonNull(url).toURI());
 
-        Map<String, Integer> wires = new HashMap<>();
+        Map<String, Instruction> wires = new HashMap<>();
 
-        Files.lines(path).forEach(line -> {
+        Files.lines(path).forEach(line -> parse(wires, line));
 
-            var instruction = parse(line);
-            System.out.println(instruction);
+        int result = evaluate(wires, "a");
 
-            switch (instruction.op) {
-                case "CONST" -> {
-                    var arg = instruction.args.get(0);
-                    wires.put(instruction.dest, constant(wires, arg));
-                }
-                case "NOT" -> {
-                    var arg = instruction.args.get(0);
-                    wires.put(instruction.dest, not(wires, arg));
-                }
-                case "AND" -> {
-                    var argA = instruction.args.get(0);
-                    var argB = instruction.args.get(1);
-                    wires.put(instruction.dest, and(wires, argA, argB));
-                }
-                case "OR" -> {
-                    var argA = instruction.args.get(0);
-                    var argB = instruction.args.get(1);
-                    wires.put(instruction.dest, or(wires, argA, argB));
-                }
-                case "RSHIFT" -> {
-                    var argA = instruction.args.get(0);
-                    var argB = instruction.args.get(1);
-                    wires.put(instruction.dest, rshift(wires, argA, argB));
-                }
-                case "LSHIFT" -> {
-                    var argA = instruction.args.get(0);
-                    var argB = instruction.args.get(1);
-                    wires.put(instruction.dest, lshift(wires, argA, argB));
-                }
-                default -> throw new RuntimeException("Unknown operation: '" + instruction.op + "'");
+        System.out.println("part 1 solution: " + result);
+
+        wires.values().forEach(v -> v.value = RESET);
+        wires.get("b").value = result;
+
+        result = evaluate(wires, "a");
+
+        System.out.println("part 2 solution: " + result);
+    }
+
+    private static int evaluate(Map<String, Instruction> wires, String thisWire) {
+
+        var ins = wires.get(thisWire);
+
+        if (ins.value == RESET) {
+
+            var op = ins.op;
+
+            var arg0 = 0;
+            if (ins.args.size() > 0) {
+                arg0 = isValue(ins.args.get(0)) ? Integer.parseInt(ins.args.get(0)) : evaluate(wires, ins.args.get(0));
             }
-        });
 
-        System.out.println(wires);
+            var arg1 = 0;
+            if (ins.args.size() > 1) {
+                arg1 = isValue(ins.args.get(1)) ? Integer.parseInt(ins.args.get(1)) : evaluate(wires, ins.args.get(1));
+            }
+
+            ins.value = switch (op) {
+                case "ASSIGN" -> arg0;
+                case "NOT" -> ~arg0 & 0xffff;
+                case "OR" -> (arg0 | arg1) & 0xffff;
+                case "AND" -> (arg0 & arg1) & 0xffff;
+                case "RSHIFT" -> (arg0 >> arg1) & 0xffff;
+                case "LSHIFT" -> (arg0 << arg1) & 0xffff;
+                default -> throw new RuntimeException("Unknown operation: '" + op + "'");
+            };
+        }
+
+        return ins.value;
     }
 
-    private static Integer not(Map<String, Integer> wires, String arg) {
-        return ~(arg.matches("[0-9]+") ? Integer.valueOf(arg) : wires.get(arg)) & 0xffff;
+    private static boolean isValue(String a) {
+        return a.matches("[0-9]+");
     }
 
-    private static Integer constant(Map<String, Integer> wires, String arg) {
-        return arg.matches("[0-9]+") ? Integer.valueOf(arg) : wires.get(arg);
-    }
-
-    private static Integer and(Map<String, Integer> wires, @NonNull String argA, @NonNull String argB) {
-
-        var a = (argA.matches("[0-9]+")) ? Integer.valueOf(argA) : wires.get(argA);
-        var b = (argB.matches("[0-9]+")) ? Integer.valueOf(argB) : wires.get(argB);
-
-        return a & b;
-    }
-
-    private static Integer or(Map<String, Integer> wires, @NonNull String argA, @NonNull String argB) {
-
-        var a = (argA.matches("[0-9]+")) ? Integer.valueOf(argA) : wires.get(argA);
-        var b = (argB.matches("[0-9]+")) ? Integer.valueOf(argB) : wires.get(argB);
-
-        return a | b;
-    }
-
-    private static Integer rshift(Map<String, Integer> wires, @NonNull String argA, @NonNull String argB) {
-
-        var a = (argA.matches("[0-9]+")) ? Integer.valueOf(argA) : wires.get(argA);
-        var b = (argB.matches("[0-9]+")) ? Integer.valueOf(argB) : wires.get(argB);
-
-        return a >> b;
-    }
-
-    private static Integer lshift(Map<String, Integer> wires, @NonNull String argA, @NonNull String argB) {
-
-        var a = (argA.matches("[0-9]+")) ? Integer.valueOf(argA) : wires.get(argA);
-        var b = (argB.matches("[0-9]+")) ? Integer.valueOf(argB) : wires.get(argB);
-
-        return a << b;
-    }
-
-    private static Instruction parse(String line) {
+    private static void parse(Map<String, Instruction> wires, String line) {
 
         var parts = line.split(" ");
         var instruction = Instruction.builder();
 
-        instruction.dest(parts[parts.length - 1].toLowerCase());
+        var destination = (parts[parts.length - 1].toLowerCase());
 
         switch (parts.length) {
             case 3 -> {
-                instruction.op("CONST");
+                instruction.op("ASSIGN");
                 instruction.args(List.of(parts[0].toLowerCase()));
             }
             case 4 -> {
@@ -126,14 +93,21 @@ public class AssemblyRequired {
             default -> throw new RuntimeException("Unknown instruction: '" + line + "'");
         }
 
-        return instruction.build();
+        wires.put(destination, instruction.build());
     }
 
-    @Builder @ToString
+    private static final Integer RESET = null;
+
+    @Builder
     private static class Instruction {
 
-        String op;          // Instruction type: CONST|NOT|AND|OR|LSHIFT|RSHIFT
-        List<String> args;  // one or two arguments for the op, may be a constant or a wire label
-        String dest;        // the destination wire of the instruction, always a wire label
+        @Builder.Default
+        String op = "";        // Instruction type: ASSIGN|NOT|AND|OR|LSHIFT|RSHIFT
+
+        @Builder.Default
+        List<String> args = List.of();  // one or two arguments for the op
+
+        @Builder.Default
+        Integer value = RESET; // RESET, if not evaluated; otherwise a positive 32-bit integer, or 0
     }
 }

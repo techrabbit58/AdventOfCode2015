8d84554829153967dfe58b9b20019fbe3c3d405d
techrabbit58 <dzb@pruefbit.de>
2021-12-19 18:54:12 +0100

    puzzle 15 part 1 w.i.p. but not done at all

--- src/com/example/Day15/CookieRecipeOptimization.java
+++ src/com/example/Day15/CookieRecipeOptimization.java
@@ -4,6 +4,8 @@ import lombok.SneakyThrows;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CookieRecipeOptimization {
@@ -11,9 +13,28 @@ public class CookieRecipeOptimization {
     @SneakyThrows
     public static void main(String[] args) {
 
-        var url = CookieRecipeOptimization.class.getResource("/ingredients.txt");
+        var url = CookieRecipeOptimization.class.getResource("/ingredients_for_test.txt");
         var path = Paths.get(Objects.requireNonNull(url).toURI());
 
-        Files.lines(path).forEach(System.out::println);
+        List<Ingredient> ingredients = new ArrayList<>();
+        Files.lines(path).forEach(line -> parse(line, ingredients));
+
+        ingredients.forEach(System.out::println);
+    }
+
+    private static void parse(String line, List<Ingredient> ingredients) {
+
+        var parts = line.split("[:,]*[\\s]+");
+
+        var ingredient = Ingredient.builder()
+                .name(parts[0].replace(":", ""))
+                .capacity(Integer.parseInt(parts[2]))
+                .durability(Integer.parseInt(parts[4]))
+                .flavor(Integer.parseInt(parts[6]))
+                .texture(Integer.parseInt(parts[8]))
+                .calories(Integer.parseInt(parts[10]))
+                .build();
+
+        ingredients.add(ingredient);
     }
 }
---
+++ src/com/example/Day15/Ingredient.java
@@ -0,0 +1,20 @@
+package com.example.Day15;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * E X A M P L E
+ * Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+ */
+@Data
+@Builder
+public class Ingredient {
+
+    private String name;
+    private int capacity;
+    private int durability;
+    private int flavor;
+    private int texture;
+    private int calories;
+}

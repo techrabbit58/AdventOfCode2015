7e1f243da958631fb66acb3f407ab043a58b0744
techrabbit58 <dzb@pruefbit.de>
2021-12-21 10:52:16 +0100

    puzzle 18 started but not done at all

--- src/com/example/Day18/LightingAnimation.java
+++ src/com/example/Day18/LightingAnimation.java
@@ -1,10 +1,11 @@
 package com.example.Day18;
 
+import static com.example.Helpers.Miscellaneous.printHeading;
+
 public class LightingAnimation {
 
     public static void main(String[] args) {
 
-        System.out.println("Advent of Code 2015");
-        System.out.println("Day 18: Like a GIF For Your Yard");
+        printHeading("Day 18: Like a GIF For Your Yard");
     }
 }
---
+++ src/com/example/Helpers/Miscellaneous.java
@@ -0,0 +1,10 @@
+package com.example.Helpers;
+
+public class Miscellaneous {
+
+    public static void printHeading(String heading) {
+        System.out.println("Advent of Code 2015");
+        System.out.println(heading);
+        System.out.println("~".repeat(heading.length()));
+    }
+}

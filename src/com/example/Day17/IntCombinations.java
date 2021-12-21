fab5ae0f9c7169b0dec28d398744c116c81a59fb
techrabbit58 <dzb@pruefbit.de>
2021-12-21 10:23:50 +0100

    puzzle 17 part 1 and part 2 done

---
+++ src/com/example/Day17/IntCombinations.java
@@ -0,0 +1,29 @@
+package com.example.Day17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Ref.: https://www.baeldung.com/java-combinations-algorithm
+ */
+public class IntCombinations {
+
+    public static List<int[]> generate(int n, int r) {
+
+        var combinations = new ArrayList<int[]>();
+        combine(combinations, new int[r], 0, n - 1, 0);
+        return combinations;
+    }
+
+    private static void combine(ArrayList<int[]> combinations, int[] ints, int start, int end, int index) {
+
+        if (index == ints.length) {
+            int[] combination = ints.clone();
+            combinations.add(combination);
+        } else if (start <= end) {
+            ints[index] = start;
+            combine(combinations, ints, start + 1, end, index + 1);
+            combine(combinations, ints, start + 1, end, index);
+        }
+    }
+}
--- src/com/example/Day17/TooMuchEggnog.java
+++ src/com/example/Day17/TooMuchEggnog.java
@@ -3,16 +3,31 @@ package com.example.Day17;
 import lombok.SneakyThrows;
 
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class TooMuchEggnog {
 
-    @SneakyThrows
     public static void main(String[] args) {
 
-        var url = TooMuchEggnog.class.getResource("/container_sizes_for_test.txt");
+        System.out.println("Advent of Code 2015 - Day 17");
+
+        var result = solution(25, "/container_sizes_for_test.txt");
+        assertEquals(4, result[0]);
+        assertEquals(3, result[1]);
+
+        result = solution(150, "/container_sizes.txt");
+        System.out.println("part 1 solution: " + result[0]);
+        System.out.println("part 2 solution: " + result[1]);
+    }
+
+    private static <T> void assertEquals(T expected, T actual) {
+        if (!expected.equals(actual)) throw new AssertionError("part 1 test failed");
+    }
+
+    @SneakyThrows
+    private static int[] solution(int necessaryVolume, String containerSizes) {
+
+        var url = TooMuchEggnog.class.getResource(containerSizes);
         var path = Paths.get(Objects.requireNonNull(url).toURI());
         var scanner = new Scanner(path);
 
@@ -21,6 +36,27 @@ public class TooMuchEggnog {
             containers.add(scanner.nextInt());
         }
 
-        System.out.println(containers);
+        var n = containers.size();
+        List<int[]> combinations = new ArrayList<>();
+
+        for (var r = 1; r <= n; r++) combinations.addAll(IntCombinations.generate(n, r));
+
+        var numberOfValidCombinations = new int[2];
+        var numContainersUsed = new HashMap<Integer, Integer>();
+
+        for (int[] combination : combinations) {
+
+            var totalVolume = 0;
+            for (var i : combination) totalVolume += containers.get(i);
+            if (totalVolume == necessaryVolume) {
+                numberOfValidCombinations[0] += 1;
+                var len = combination.length;
+                numContainersUsed.put(len, numContainersUsed.getOrDefault(len, 0) + 1);
+            }
+        }
+        numberOfValidCombinations[1] = numContainersUsed.get(
+                numContainersUsed.keySet().stream().reduce(Integer::min).orElse(-1));
+
+        return numberOfValidCombinations;
     }
 }

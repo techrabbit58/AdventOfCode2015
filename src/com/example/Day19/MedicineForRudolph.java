7d804bbd47f87220f3674e68408559f2cb40117c
techrabbit58 <dzb@pruefbit.de>
2021-12-22 20:03:52 +0100

    day 19 part 1 done, part 2 w.i.p. (tests ok already, but too slow)

--- src/com/example/Day19/MedicineForRudolph.java
+++ src/com/example/Day19/MedicineForRudolph.java
@@ -1,4 +1,115 @@
 package com.example.Day19;
 
+import com.example.Helpers.Assertions;
+import lombok.SneakyThrows;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class MedicineForRudolph {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+
+        // T E S T   P A R T 1
+        Assertions.assertEquals(4, calibrate("H => HO\nH => OH\nO => HH\n\nHOH"));
+        Assertions.assertEquals(7, calibrate("H => HO\nH => OH\nO => HH\n\nHOHOHO"));
+
+        // T E S T   P A R T 2
+        Assertions.assertEquals(3,
+                generateMedicineMolecule("e => H\ne => O\nH => HO\nH => OH\nO => HH\n\nHOH"));
+        Assertions.assertEquals(6,
+                generateMedicineMolecule("e => H\ne => O\nH => HO\nH => OH\nO => HH\n\nHOHOHO"));
+
+        // S O L U T I O N   P A R T 1
+
+        var url = MedicineForRudolph.class.getResource("/molecule_grammar.txt");
+        var path = Paths.get(Objects.requireNonNull(url).toURI());
+
+        var part1Solution = calibrate(Files.readString(path));
+        System.out.println("part 1 solution: " + part1Solution);
+
+        var part2Solution = generateMedicineMolecule(Files.readString(path));
+        System.out.println("part 2 solution: " + part2Solution);
+    }
+
+    private static int calibrate(String input) {
+
+        Set<String> grammar = new HashSet<>();
+        var word = parse(input, grammar);
+
+        Set<String> molecules = new HashSet<>();
+        findDistinctMolecules(grammar, word, molecules);
+
+        return molecules.size();
+    }
+
+    private static int generateMedicineMolecule(String input) {
+
+        Set<String> grammar = new HashSet<>();
+        var word = parse(input, grammar);
+
+        int counter = 0;
+        List<Set<String>> molecules = new ArrayList<>();
+        molecules.add(new HashSet<>());
+        molecules.get(0).add("e");
+
+        while (!molecules.get(counter).contains(word)) {
+            counter += 1;
+            var words = molecules.get(counter - 1);
+            var newMolecules = new HashSet<String>();
+            for (var m : words) findDistinctMolecules(grammar, m, newMolecules);
+            var maxCommonPrefix = 0;
+            for (var s : newMolecules) maxCommonPrefix = Integer.max(maxCommonPrefix, commonPrefix(word, s));
+            Set<String> candidates = new HashSet<>();
+            for (var s : newMolecules) if (maxCommonPrefix == commonPrefix(s, word)) candidates.add(s);
+            molecules.add((candidates.size() < 4) ? newMolecules : candidates);
+            System.out.println("counter=" + counter +
+                    ", commonPrefix=" + maxCommonPrefix + ", numCandidates=" + candidates.size());
+        }
+
+        return counter;
+    }
+
+    private static int commonPrefix(String s, String t) {
+
+        var sLen = s.length();
+        var tLen = t.length();
+        var len = Math.min(sLen, tLen);
+
+        for (var i = 0; i < len; i++) if (s.charAt(i) != t.charAt(i)) return i + 1;
+        return len;
+    }
+
+    private static void findDistinctMolecules(Set<String> grammar, String word, Set<String> molecules) {
+        grammar.forEach(line -> {
+            var rule = line.split(" => ");
+            var index = 0;
+            while ((index = word.indexOf(rule[0], index)) >= 0) {
+                var head = word.substring(0, index);
+                var tail = word.substring(index + rule[0].length());
+                var newMolecule = head + rule[1] + tail;
+                molecules.add(newMolecule);
+                index += rule[0].length();
+            }
+        });
+    }
+
+    private static String parse(String input, Set<String> grammar) {
+
+        String word;
+
+        var scanner = new Scanner(input);
+        while (scanner.hasNextLine()) {
+            var line = scanner.nextLine();
+            if (line.length() == 0) break;
+            grammar.add(line);
+        }
+
+        Assertions.assertTrue(scanner.hasNextLine(), "scanner: missing word");
+
+        word = scanner.nextLine();
+        return word;
+    }
 }
--- src/com/example/Helpers/Assertions.java
+++ src/com/example/Helpers/Assertions.java
@@ -8,4 +8,19 @@ public class Assertions {
             throw new AssertionError("test failed");
         }
     }
+
+    public static <T> void assertEquals(final T expected, final T actual, final String message) {
+        if (!expected.equals(actual)) {
+            System.err.println(message);
+            System.err.println("expected '" + expected + "', got '" + actual + "'");
+            throw new AssertionError("test failed");
+        }
+    }
+
+    public static void assertTrue(final boolean predicate, final String message) {
+        if (!predicate) {
+            System.err.println(message);
+            throw new AssertionError("test failed");
+        }
+    }
 }

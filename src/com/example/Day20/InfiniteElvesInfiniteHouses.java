37049941b3b269826b575b7a514ee21a8e08047d
techrabbit58 <dzb@pruefbit.de>
2021-12-29 12:17:20 +0100

    day 20 part 1 and 2 done

--- src/com/example/Day20/InfiniteElvesInfiniteHouses.java
+++ src/com/example/Day20/InfiniteElvesInfiniteHouses.java
@@ -2,21 +2,75 @@ package com.example.Day20;
 
 import com.example.Helpers.Miscellaneous;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InfiniteElvesInfiniteHouses {
+
+    private static final int PUZZLE_INPUT = 34_000_000;
 
     public static void main(String[] args) {
 
         Miscellaneous.printHeading("Day 20: Infinite Elves and Infinite Houses");
 
-        final var limit = 34_000_000 / 10;
+        System.out.println("part 1 solution: " + part1());
+        System.out.println("part 2 solution: " + part2());
+    }
 
-        for (var house = 1; house < 10; house++) {
-            var numPresents = 1;
-            for (var elf = 2; elf <= (house + 1); elf++) {
-                if (house % elf == 0) numPresents += elf;
-            }
-            System.out.println("house=" + house + ", numPresents=" + numPresents * 10);
-            if (numPresents >= limit) break;
+    private static int part2() {
+
+        var house = 1;
+        while(true) {
+            var numPresents = 11 * modifiedSigma(house);
+            if (numPresents >= PUZZLE_INPUT) break;
+            house += 1;
         }
+
+        return house;
+    }
+
+    private static int part1() {
+
+        var house = 1;
+        while (true) {
+            int numPresents = 10 * sigma(house);
+            if (numPresents >= PUZZLE_INPUT) break;
+            house += 1;
+        }
+
+        return house;
+    }
+
+    /**
+     * The sigma() function. It is the sum of all divisors of n. (Divisors include 1 and n!)
+     */
+    private static int sigma(final int n) {
+        return getDivisors(n).stream().reduce(Integer::sum).orElse(0);
+    }
+
+    /**
+     * A variant of the sigma() function that limits the responsibility to 50 houses per elf,
+     * by applying a filter stage before the reduction stage of the stream processing.
+     */
+    private static int modifiedSigma(final int n) {
+        return getDivisors(n).stream().filter(d -> n < d * 50).reduce(Integer::sum).orElse(0);
+    }
+
+    /**
+     * Find all divisors of integer n, including 1 and n itself.
+     */
+    private static Set<Integer> getDivisors(final int n) {
+
+        Set<Integer> divisors = new HashSet<>();
+
+        int limit = (int) (1 + Math.sqrt(n));
+        for (var d = 1; d < limit; d++) {
+            if (n % d == 0) {
+                divisors.add(d);
+                divisors.add(n / d);
+            }
+        }
+
+        return divisors;
     }
 }

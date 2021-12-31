a16e4399b611aa9d506d15d88322bbaf4a7bcbfc
techrabbit58 <dzb@pruefbit.de>
2021-12-31 18:48:12 +0100

    day 22 part 1 w.i.p.

--- src/com/example/Day22/WizardSimulator.java
+++ src/com/example/Day22/WizardSimulator.java
@@ -2,23 +2,47 @@ package com.example.Day22;
 
 import com.example.Helpers.Miscellaneous;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class WizardSimulator {
+
+    private static final Globals global = Globals.getInstance();
 
     public static void main(String[] args) {
 
         Miscellaneous.printHeading("Day 22: Wizard Simulator 20XX");
 
-        var globals = Globals.getInstance();
+        // T E S T   P A R T 1
 
-        var player = new Wizard(globals.TEST_INPUT_PLAYER[0]);
-        player.addSpellToScript("Poison");
+        var battleState = parse(global.TEST_INPUT_PLAYER[0], global.TEST_INPUT_BOSS[0]);
 
-        var boss = new Boss(globals.TEST_INPUT_BOSS[0]);
+        System.out.println(battleState);
+    }
 
-        var gameMaster = new GameMaster();
-        gameMaster.addPlayer(player);
-        gameMaster.addPlayer(boss);
+    private static BattleState parse(String playerInit, String bossInit) {
 
-        gameMaster.run();
+        var playerProperties = parseInput(playerInit);
+        var bossProperties = parseInput(bossInit);
+
+        return new BattleState()
+                .setPlayerHealthPoints(playerProperties.getOrDefault("Hit Points", 0))
+                .setPlayerMana(playerProperties.getOrDefault("Mana", 0))
+                .setBossHealthPoints(bossProperties.getOrDefault("Hit Points", 0))
+                .setBossDamage(bossProperties.getOrDefault("Damage", 0));
+    }
+
+    private static Map<String, Integer> parseInput(String init) {
+
+        return init.lines()
+                .map(s -> s.split(":\\s+"))
+                .collect(Collectors.toMap(
+                        a -> a[0],
+                        a -> Integer.parseInt(a[1])
+                ));
+    }
+
+    private static boolean isPlayerWinner(BattleState battleState) {
+        return battleState.hasBattleEnded() && battleState.getPlayerHealthPoints() > 0;
     }
 }
